9bc2a97b91e39e20284e161669c12149411bd5db
Daniel Schroeder <kriegerfaust88@gmail.com>
2020-04-10 11:50:43 -0700

    Added ThymeLeaf dependency and 'books' template

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -97,7 +97,6 @@ public class Book {
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
                 ", publisher=" + publisher +
-                ", authors=" + authors +
                 '}';
     }
 }
